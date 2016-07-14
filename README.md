f225eabe39cf3b744afcb6adea534708f43c8a27
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2016-07-14 21:10:06 +0200

    Initial repository

---
+++ README.md
@@ -0,0 +1,276 @@
+# Julia interface to SAOImage/DS9
+
+This [Julia](http://julialang.org/) package provides an interface to the image
+viewer [SAOImage/DS9](http://ds9.si.edu/site/Home.html) via the
+[XPA Messaging System](https://github.com/ericmandel/xpa).
+
+
+## Prerequisites
+
+To use this package, *DS9* and *XPA* must be installed on your computer.  If
+this is not the case, they are available for different operating systems.  For
+example, on Ubuntu, just do:
+
+    sudo apt-get install saods9
+
+Optionally, you may want to install `IPC.jl` package to benefit from shared
+memory.  For now, this package at https://github.com/emmt/IPC.jl
+
+In your Julia code/session, it is sufficient to do:
+
+    import DS9
+
+or:
+
+    using DS9
+
+which are equivalent as `DS9.jl` does not export any symbols.  Thus all
+commands are prefixed by `DS9.`, if you prefer a different prefix, you can do
+something like:
+
+    const ds9 = DS9
+
+
+## General syntax for the requests
+
+The general syntax to issue a *set* request to the current DS9 access point is:
+
+    DS9.set(args...; data=nothing)
+
+where `args...` are any number of arguments which will be automatically
+converted in a string where the arguments are separated by spaces.  The keyword
+`data` may be used to specify the data to send with the request, it may be
+`nothing` (the default) or a Julia array.  For instance, the following 3
+statements will set the current zoom to be equal to 3.7:
+
+    DS9.set(:zoom,:to,3.7)
+    DS9.set("zoom to",3.7)
+    DS9.set("zoom to 3.7")
+
+where the last line shows the string which is effectively sent to DS9 via the
+`xpa_set` method.
+
+The following methods can be used to issue a *get* request to the current DS9
+access point depending on the expected type of result:
+
+    DS9.get_bytes(args...)             -> buf
+    DS9.get_text(args...)              -> str
+    DS9.get_lines(args...; keep=false) -> arr
+    DS9.get_words(args...)             -> arr
+
+where `args...` are treated as for the `DS9.set` method.  The returned values
+are respectively a vector of bytes, a single string (with the last end of line
+removed if any), an array of strings (one for each line of the result and empty
+line removed unless keyword `keep` is set `true`), or an array of (non-empty)
+words.
+
+If a single scalar integer or floating point is expected, the following methods
+are available:
+
+    DS9.get_integer(args...)    -> scalar
+    DS9.get_float(args...)      -> scalar
+
+which return respectively an `Int` and a `Float64`.
+
+
+## Display or retrieve an image
+
+To display an image, do:
+
+    DS9.set_data(arr)
+
+where `arr` is a 2D or 3D Julia array.  If `arr` is a shared memory array
+(provided by the `IPC.jl` package), the shared memory segment is directly used
+by DS9; otherwise, the contents of the array is sent to DS9.  DS9 will display
+the data in the currently selected frame with the current scale parameters,
+zoom, orientation, rotation, etc.
+
+To retrieve the array in the current DS9 frame, do:
+
+    arr = DS9.get_data();
+
+
+## Connection to a specific DS9 instance
+
+By default, all requests are sent to the first DS9 instance found by the XPA
+name server.  To send further requests to a specific DS9 instance, you may
+do:
+
+    DS9.connect(apt) -> ident
+
+where `apt` is a string identifying a specific XPA access point.  The returned
+value is the fully qualified identifier of the access point, it has the form
+`IP:port` for a TCP/IP socket or it is the path to the socket file for an
+AF/Unix socket.  The access point `apt` may be a fully qualified identifier or
+a template of the form `class:name` like `"DS9:*"` which correspond to any
+instance of the class `"DS9"`.  Note that `name` correspond to the argument of
+the `-title` option when DS9 is launched.  See
+[XPA Template](http://hea-www.harvard.edu/RD/xpa/template.html) for a complete
+description.
+
+To retrieve the identifier of the current access point to DS9, you may call:
+
+    DS9.connection()
+
+Remember that all requests are sent to a given access point, but you may switch
+between DS9 instances.  For instance:
+
+    apt1 = DS9.connection()              # retrieve current access point
+    apt2 = DS9.connect("DS9:some_name")  # second access point
+    DS9.set_data(arr)    # send an image to apt2
+    DS9.connect(apt1);   # switch to apt1
+    DS9.zoom_to(1.4)     # set zoom in apt1
+    ...
+
+When `DS9.jl` package is imported, it automatically connects to the first
+access point matching `"DS9.*"` with a warning if no access points, or if more
+than one access point are found.
+
+
+## Using the XPA Message System
+
+Julia interface to SAOImage/DS9 is built on top of an implementation of an
+interface to use the XPA Message System by Eric Mandel.  For now, only part of
+the client routines have been interfaced with Julia.  The interface exploits
+the power of `ccall` to directly calls the routines of the compiled XPA
+library.  The implemented methods are described in what follows, more extensive
+XPA documentation can be found
+[here](http://hea-www.harvard.edu/RD/xpa/help.html).
+
+
+### Get data
+
+The method:
+
+    xpa_get(src [, params...]) -> tup
+
+retrieves data from one or more XPA access points identified by `src` (a
+template name, a `host:port` string or the name of a Unix socket file) with
+parameters `params` (automatically converted into a single string where the
+parameters are separated by a single space).  The result is a tuple of tuples
+`(data,name,mesg)` where `data` is a vector of bytes (`UInt8`), `name` is a
+string identifying the server which answered the request and `mesg` is an error
+message (a zero-length string `""` if there are no errors).
+
+The following keywords are accepted:
+
+* `nmax` specifies the maximum number of answers, `nmax=1` by default.
+  Use `nmax=-1` to use the maximum number of XPA hosts.
+
+* `xpa` specifies an XPA handle (created by `xpa_open`) for faster connections;
+
+* `mode` specifies options in the form `"key1=value1,key2=value2"`.
+
+
+There are more simple methods which return only the data part of the answer,
+possibly after conversion.  These methods limit the number of answers to be at
+most one and throw an error if `xpa_get` returns a non-empty error message.
+To retrieve the `data` part of the answer received by an `xpa_get` request as a
+vector of bytes, call the method:
+
+    xpa_get_bytes(src [, params...]; xpa=..., mode=...) -> buf
+
+where arguments `src` and `params...` and keywords `xpa` and `mode` are passed
+to `xpa_get`.  To convert the result of `xpa_get_bytes` into a single string,
+call the method:
+
+    xpa_get_text(src [, params...]; xpa=..., mode=...) -> str
+
+To split the result of `xpa_get_text` into an array of strings, one for each
+line, call the method:
+
+    xpa_get_lines(src [, params...]; keep=false, xpa=..., mode=...) -> arr
+
+where keyword `keep` can be set `true` to keep empty lines.  Finally, to split
+the result of `xpa_get_text` into an array of words, call the method:
+
+    xpa_get_words(src [, params...]; xpa=..., mode=...) -> arr
+
+
+### Send data or commands
+
+The method:
+
+    xpa_set(dest [, params...]; data=nothing) -> tup
+
+send `data` to one or more XPA access points identified by `dest` with
+parameters `params` (automatically converted into a single string where the
+parameters are separated by a single space).  The result is a tuple of tuples
+`(name,mesg)` where `name` is a string identifying the server which received
+the request and `mesg` is an error message (a zero-length string `""` if there
+are no errors).
+
+The following keywords are accepted:
+
+* `data` the data to send, may be `nothing` or an array.  If it is an array, it
+  must be an instance of a sub-type of `DenseArray` which implements the
+  `pointer` method.
+
+* `nmax` specifies the maximum number of answers, `nmax=1` by default.
+  Use `nmax=-1` to use the maximum number of XPA hosts.
+
+* `xpa` specifies an XPA handle (created by `xpa_open`) for faster connections;
+
+* `mode` specifies options in the form `"key1=value1,key2=value2"`.
+
+* `check` specifies whether to check for errors.  If this keyword is set true,
+  an error is thrown for the first non-empty error message `mesg` encountered
+  in the list of answers.
+
+
+## Open a persistent client connection
+
+The method:
+
+    xpa_open() -> handle
+
+returns a handle to an XPA permanent connection and which can be used as the
+argument of the `xpa` keyword of the `xpa_get` and `xpa_set` methods to speed
+up requests.  The handle is automatically closed when the handle is finalized
+by the garbage collector.
+
+
+## Utilities
+
+The method:
+
+    xpa_list(; xpa=...) -> arr
+
+returns a list of the existing XPA access points as an array of structured
+elements:
+
+    arr[i].class    # class of the access point
+    arr[i].name     # name of the access point
+    arr[i].addr     # socket address
+    arr[i].user     # user name of access point owner
+    arr[i].access   # allowed access (g=xpaget,s=xpaset,i=xpainfo)
+
+all members but `access` are strings, the `addr` member is the name of the
+socket used for the connection (either `host:port` for internet socket, or a
+file path for local unix socket), `access` is a combination of the bits
+`XPA.GET`, `XPA.SET` and/or `XPA.INFO` depending whether `xpa_get`, `xpa_set`
+and/or `xpa_info` access are granted.  Note that `xpa_info` is not yet
+implemented.
+
+XPA messaging system can be configured via environment variables.  The
+method `xpa_config` provides means to get or set XPA settings.  With a
+single argument:
+
+    xpa_config(key) -> val
+
+yields the current value of the XPA parameter `key` which is one of:
+
+    :XPA_MAXHOSTS
+    :XPA_SHORT_TIMEOUT
+    :XPA_LONG_TIMEOUT
+    :XPA_CONNECT_TIMEOUT
+    :XPA_TMPDIR
+    :XPA_VERBOSITY
+    :XPA_IOCALLSXPA
+
+The key may be a symbol or a string, the value of a parameter may be a boolean,
+an integer or a string.  To set an XPA parameter, call the method:
+
+    xpa_config(key, val) -> old
+
+which returns the previous value of the parameter.
---
+++ src/DS9.jl
@@ -0,0 +1,415 @@
+#
+# DS9.jl --
+#
+# Implement communication with SAOImage DS9 (http://ds9.si.edu) via the XPA
+# protocol.
+#
+#------------------------------------------------------------------------------
+#
+# This file is part of DS9.jl released under the MIT "expat" license.
+# Copyright (C) 2016, Éric Thiébaut (https://github.com/emmt).
+#
+module DS9
+
+warning(msg...) = print_with_color(:yellow, STDERR, "WARNING: ", msg..., "\n")
+
+using XPA
+const HAVE_IPC = try
+    using IPC
+    true
+catch e
+    warning("module IPC not found, will not use shared memory")
+    false
+end
+
+typealias PixelTypes Union{UInt8,Int16,Int32,Int64,Float32,Float64}
+const PIXEL_TYPES = (UInt8, Int16, Int32, Int64, Float32, Float64)
+
+_match(pat::AbstractString, str::AbstractString) = (pat == str)
+_match(::Void, str::AbstractString) = true
+_match(pat::Regex, str::AbstractString) = is_match(pat, str)
+
+function search(;name::Union{Void,AbstractString,Regex}=nothing,
+                user::Union{Void,AbstractString,Regex}=ENV["USER"],
+                access::Integer=0)
+    access = UInt(access) & (XPA.GET|XPA.SET|XPA.INFO)
+    for apt in xpa_list()
+        apt.class == "DS9" || continue
+        (apt.access & access) == access || continue
+        _match(name, apt.name) || continue
+        _match(user, apt.user) || continue
+        return apt.addr
+    end
+    nothing
+end
+
+# Private variables to store current DS9 access point and XPA connection.
+_DS9 = ""
+_XPA = XPA.NullHandle
+
+"""
+   DS9.connect(apt="DS9:*") -> ident
+
+set DS9 access point for further DS9 commands.  Argument `apt` identifies the
+access point, it can be a template string like "DS9:*" which is the default
+value.  The returned value is the name of the access point.
+
+To retrieve the name of the current DS9 access point, do:
+
+   DS9.connection() -> currentname
+"""
+function connect(apt::AbstractString="DS9:*")
+    global _DS9, _XPA
+    if _XPA == XPA.NullHandle
+        _XPA = xpa_open()
+    end
+    cnt = 0
+    for (data, name, mesg) in xpa_get(apt, "version"; xpa=_XPA, nmax=-1)
+        length(mesg) > 0 && continue # ignore errors
+        cnt += 1
+        if cnt == 1
+            _DS9 = split(name)[end]
+        end
+    end
+    if cnt > 1
+        warning("more than one matching DS9 server found, ",
+                "the first one ($_DS9) was selected")
+    elseif cnt == 0
+        _DS9 = "DS9:*"
+        warning("no matching DS9 server found, the default \"", _DS9,
+                "\" will be used")
+    end
+    return _DS9
+end
+
+connection() = _DS9
+
+@doc @doc(connect) connection
+
+# establish the first connection
+connect();
+
+get_bytes(args...) = xpa_get_bytes(_DS9, args...; xpa=_XPA)
+
+get_text(args...) = chomp(xpa_get_text(_DS9, args...; xpa=_XPA))
+
+get_lines(args...; keep::Bool=false) =
+    xpa_get_lines(_DS9, args...; xpa=_XPA, keep=keep)
+
+get_words(args...) = xpa_get_words(_DS9, args...; xpa=_XPA)
+
+get_integer(args...) = parse(Int, get_text(args...))
+
+get_float(args...) = parse(Float64, get_text(args...))
+
+set(args...; data::Union{Void,AbstractArray}=nothing) =
+    (xpa_set(_DS9, args...; xpa=_XPA, check=true, data=data); nothing)
+
+about() = get_text("about")
+
+version() = get_text("version")
+
+exit() = set("exit")  # FIXME: should reconnect...
+
+iconify() = yesorno(get_text("iconify"))
+
+iconify(value::Bool) = iconify(yesorno(value))
+
+iconify(value::Union{Symbol,AbstractString}) =
+    set("iconify", value)
+
+lower() = set("lower")
+
+raise() = set("raise")
+
+zoom() = get_float("zoom")
+
+zoom(value::Real) = set("zoom", value)
+
+zoom_to(value::Real) = set("zoom to", value)
+
+zoom_to_fit() = set("zoom to fit")
+
+rotate() = get_float("rotate")
+
+rotate(value::Real) = set("rotate", value)
+
+rotate_to(value::Real) = set("rotate to", value)
+
+rotate_open() = set("rotate open")
+
+rotate_close() = set("rotate close")
+
+orient() = get_text("orient")
+
+orient(value::Union{AbstractString,Symbol}) = set("orient", value)
+
+#iexam(;event::Union{AbstractString,Symbol}=:button) = get_words("iexam", event)
+
+iexam(args...; event::Union{AbstractString,Symbol}=:button) =
+    get_words("iexam", event, args...)
+
+get_threads() = get_integer(:threads)
+set_threads(n::Integer) = set(:threads, n)
+
+function guiwidth end
+function guiheight end
+
+
+"""
+To retrieve the dimensions of the GUI:
+
+    DS9.guiwidth()  -> width
+    DS9.guiheight() -> height
+
+To set the dimensions of the GUI:
+
+    DS9.guiwidth(width)   -> width
+    DS9.guiheight(height) -> height
+
+"""
+guiwidth
+
+@doc @doc(guiwidth) guiheight
+
+for s in ("width", "height")
+    f = symbol(:gui, s)
+    @eval begin
+        $f() = get_integer($s)
+        function $f(value::Integer)
+            value::Int = value
+            set($s, value)
+            return value
+        end
+    end
+
+end
+
+"""
+# Get DS9 frame settings
+
+    DS9.getframe(           # returns the id of the current frame
+    DS9.getframe("frameno") # returns the id of the current frame
+    DS9.getframe("all")     # returns the id of all frames
+    DS9.getframe("active")  # returns the id of all active frames
+    DS9.getframe("lock")
+    DS9.getframe("has","amplifier")
+    DS9.getframe("has","datamin")
+    DS9.getframe("has","datasec")
+    DS9.getframe("has","detector")
+    DS9.getframe("has","grid")
+    DS9.getframe("has","iis")
+    DS9.getframe("has","irafmin")
+    DS9.getframe("has","physical")
+    DS9.getframe("has","smooth")
+    DS9.getframe("has","contour")
+    DS9.getframe("has","contour","aux")
+    DS9.getframe("has","fits")
+    DS9.getframe("has","fits","bin")
+    DS9.getframe("has","fits","cube")
+    DS9.getframe("has","fits","mosaic")
+    DS9.getframe("has","marker","highlite")
+    DS9.getframe("has","marker","paste")
+    DS9.getframe("has","marker","select")
+    DS9.getframe("has","marker","undo")
+    DS9.getframe("has","system","physical")
+    DS9.getframe("has","wcs","wcsa")
+    DS9.getframe("has","wcs","equatorial","wcsa")
+    DS9.getframe("has","wcs","linear","wcsa")
+
+"""
+getframe(args...) = get_words("frame", args...)
+
+currentframe() = get_integer("frame")
+
+framehas(what...) = yesorno(get_text("frame", "has", what...))
+
+"""
+# Set DS9 frame settings
+
+    DS9.setframe("center")         # center current frame
+    DS9.setframe("center",1)       # center 'Frame1'
+    DS9.setframe("center","all")   # center all frames
+    DS9.setframe("clear")          # clear current frame
+    DS9.setframe("new")            # create new frame
+    DS9.setframe("new","rgb")      # create new rgb frame
+    DS9.setframe("delete")         # delete current frame
+    DS9.setframe("reset")          # reset current frame
+    DS9.setframe("refresh")        # refresh current frame
+    DS9.setframe("hide")           # hide current frame
+    DS9.setframe("show",1)         # show frame 'Frame1'
+    DS9.setframe("move","first")   # move frame to first in order
+    DS9.setframe("move","back")    # move frame back in order
+    DS9.setframe("move","forward") # move frame forward in order
+    DS9.setframe("move","last")    # move frame to last in order
+    DS9.setframe("first")          # goto first frame
+    DS9.setframe("prev")           # goto prev frame
+    DS9.setframe("next")           # goto next frame
+    DS9.setframe("last")           # goto last frame
+    DS9.setframe("frameno",4)      # goto frame 'Frame4',create if needed
+    DS9.setframe(3)                # goto frame 'Frame3',create if needed
+    DS9.setframe("match","wcs")
+    DS9.setframe("lock","wcs")
+"""
+setframe(args...) = set("frame", args...)
+
+firstframe() = setframe("first")
+lastframe() = setframe("last")
+
+doc"""
+# get DS9 frame data
+
+    DS9.get_data(;endian=:native) -> arr
+
+yields the contents of current DS9 frame as an array (or as `nothing` if the
+frame is empty).
+
+"""
+function get_data(; endian::Union{Symbol,AbstractString}=:native)
+    typ = bitpix_to_type(get_bitpix())
+    if typ === Void; return nothing; end
+    siz = get_size()
+    buf = get_bytes("array", byteorder(endian))
+    reshape(reinterpret(typ, buf), siz)
+end
+
+doc"""
+
+    DS9.set_data(arr; mask=false, new=false, endian=:native)
+
+set contents of current DS9 frame to be array `arr`.
+"""
+function set_data{T<:PixelTypes,N}(arr::AbstractArray{T,N};
+                                   endian::Symbol=:native,
+                                   mask::Bool=false,
+                                   new::Bool=false)
+    args = Array(ASCIIString, 0)
+    push!(args, "array")
+    if new; push!(args, "new"); end
+    if mask; push!(args, "mask"); end
+    set(args..., arraydescriptor(arr; endian=endian); data=arr)
+end
+
+if HAVE_IPC
+    function set_data{T<:PixelTypes,N}(arr::ShmArray{T,N};
+                                       endian::Symbol=:native)
+        set("shm", "array", "shmid", shmid(arr),
+            arraydescriptor(arr; endian=endian))
+    end
+end
+
+function arraydescriptor{T,N}(arr::AbstractArray{T,N}; endian::Symbol=:native)
+    error("only 2D and 3D arrays are supported")
+end
+
+function arraydescriptor{T}(arr::AbstractArray{T,2}; endian::Symbol=:native)
+    bp = bitpixof(T)
+    bp != 0 || error("unsupported data type")
+    string("[xdim=",size(arr,1),",ydim=",size(arr,2),
+           ",bitpix=",bp,",endian=",endian,"]")
+end
+
+function arraydescriptor{T}(arr::AbstractArray{T,3}; endian::Symbol=:native)
+    bp = bitpixof(T)
+    bp != 0 || error("unsupported data type")
+    string("[xdim=",size(arr,1),",ydim=",size(arr,2),",zdim=",size(arr,3),
+           ",bitpix=",bp,",endian=",endian,"]")
+end
+
+"""
+The following calls retrieve parameters of data in current DS9 frame:
+
+    DS9.get_bitpix() -> bitpix
+    DS9.get_width()  -> width
+    DS9.get_height() -> height
+    DS9.get_depth()  -> depth
+    DS9.get_size()   -> dims
+    DS9.get_size(n)  -> nth_dims
+"""
+function get_bitpix end
+
+for field in ("bitpix", "width", "height", "depth")
+    func = symbol(:get_,field)
+    @eval function $func()
+        str = get_text("fits", $field)
+        length(str) > 0 ? parse(Int, str) : 0
+    end
+end
+
+function get_size()
+    lst = get_words("fits", "size")
+    ntuple(i -> parse(Int, lst[i]), length(lst))
+end
+
+
+function trueorfalse(str::AbstractString)
+    str == "true"  ? true  :
+    str == "false" ? false :
+    throw(ArgumentError("expecting \"true\" or \"false\""))
+end
+
+function yesorno(str::AbstractString)
+    str == "yes" ? true  :
+    str == "no"  ? false :
+    throw(ArgumentError("expecting \"yes\" or \"no\""))
+end
+
+yesorno(flag::Bool) = (flag ? "yes" : "no")
+trueorfalse(flag::Bool) = (flag ? "true" : "false")
+
+doc"""
+    DS9.bitpixof(x)
+
+yields FITS bits-per-pixel (BITPIX) value for `x` which can be an array or
+a type.  A value of 0 is returned if `x` is not of a supported type.
+"""
+function bitpixof end
+bitpixof{T,N}(::AbstractArray{T,N}) = bitpixof(T)
+for T in PIXEL_TYPES
+    bp = (T <: Integer ? 8 : -8)*sizeof(T)
+    @eval bitpixof(::Type{$T}) = $bp
+    @eval bitpixof(::$T) = $bp
+end
+bitpixof(::Any) = 0
+
+bitpix_to_type(bitpix::Int) =
+    bitpix ==   8 ? UInt8   :
+    bitpix ==  16 ? Int16   :
+    bitpix ==  32 ? Int32   :
+    bitpix ==  64 ? Int64   :
+    bitpix == -32 ? Float32 :
+    bitpix == -64 ? Float64 :
+    Void
+bitpix_to_type(bitpix::Integer) = bitpix_to_type(Int(bitpix))
+bitpix_to_type(::Any) = Void
+
+function fetchint(str::AbstractString,
+                  prefix::AbstractString="")
+    offset = length(prefix)
+    if offset > 0 && ! startswith(str, prefix)
+        error("string does not match prefix")
+    end
+    if length(str) ≤ offset
+        error("short string")
+    end
+    parse(Int, str[1+offset:end])
+end
+
+function byteorder(endian::Symbol)
+    endian == :native ? (ENDIAN_BOM == 0x01020304 ? "big" :
+                         ENDIAN_BOM == 0x04030201 ? "little" :
+                         error("unknown byte order")) :
+    endian == :big ? "big" :
+    endian == :little ? "little" :
+    error("invalid byte order")
+end
+
+function byteorder(endian::AbstractString)
+    endian == "native" ? (ENDIAN_BOM == 0x01020304 ? "big" :
+                          ENDIAN_BOM == 0x04030201 ? "little" :
+                          error("unknown byte order")) :
+    endian == "big" || endian == "little" ? endian :
+    error("invalid byte order")
+end
+
+end # module
