cd98cfc8d613eb526e78f738aaabb923ab29a0e2
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-11-17 17:14:19 +0100

    A bit more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,14 @@
+module SAOImageDS9Tests
 using Test
 using SAOImageDS9, XPA
 using XPA: TupleOf
 
 proc = Base.Process[]
 
-function ds9start(timeout::Real = 10.0)
+function ds9start(timeout::Real = 30.0)
     global proc
     elapsed = 0.0
-    seconds = 1.0
+    seconds = 0.5
     while true
         try
             return SAOImageDS9.connect()
@@ -35,7 +36,11 @@ end
 ds9start()
 
 @testset "Get requests" begin
-    @test typeof(SAOImageDS9.get(VersionNumber)) == VersionNumber
+    @test typeof(SAOImageDS9.get(VersionNumber)) === VersionNumber
+    @test typeof(SAOImageDS9.get(Int, "width")) === Int
+    @test typeof(SAOImageDS9.get(Int, "height")) === Int
 end
 
 ds9kill()
+
+end # module
