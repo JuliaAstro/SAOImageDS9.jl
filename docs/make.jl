81ec8301741376ef8aa4328064f5e54ba90620af
Abhro R <5664668+abhro@users.noreply.github.com>
2024-07-16 16:13:05 -0400

    Remove config option DEPLOYDOCS

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,15 @@
 using Documenter
 using SAOImageDS9
 
-DEPLOYDOCS = (get(ENV, "CI", nothing) == "true")
-
 include("pages.jl")
 
 makedocs(
     sitename = "Connecting to SAOImage/DS9",
-    format = Documenter.HTML(
-        prettyurls = DEPLOYDOCS,
-    ),
+    format = Documenter.HTML(),
     authors = "Éric Thiébaut and contributors",
     pages = pages,
 )
 
-if DEPLOYDOCS
-    deploydocs(
-        repo = "github.com/JuliaAstro/SAOImageDS9.jl.git",
-    )
-end
+deploydocs(
+    repo = "github.com/JuliaAstro/SAOImageDS9.jl.git",
+)
