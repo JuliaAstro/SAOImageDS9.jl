c515f65eb2bb05f05bd0fc677b9bdbb6582f40eb
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-07-09 21:03:17 +0200

    Add script to build doc.

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,18 @@
-using Documenter
+DEPLOYDOCS = (get(ENV, "CI", nothing) == "true")
+push!(LOAD_PATH, "../src/")
 using SAOImageDS9
+using Documenter
+using Documenter.Remotes: GitHub
 
 include("pages.jl")
 
-makedocs(
+makedocs(;
     modules = [SAOImageDS9],
     sitename = "SAOImageDS9.jl",
+    repo = GitHub("JuliaAstro/SAOImageDS9.jl"),
     format = Documenter.HTML(
         canonical = "https://juliaastro.org/SAOImageDS9/stable/",
+        prettyurls = DEPLOYDOCS,
     ),
     authors = "Éric Thiébaut and contributors",
     pages = pages,
@@ -15,8 +20,10 @@ makedocs(
     checkdocs = :export,
 )
 
-deploydocs(
-    repo = "github.com/JuliaAstro/SAOImageDS9.jl.git",
-    push_preview = true,
-    versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
-)
+if DEPLOYDOCS
+    deploydocs(
+        repo = "github.com/JuliaAstro/SAOImageDS9.jl.git",
+        push_preview = true,
+        versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
+    )
+end
